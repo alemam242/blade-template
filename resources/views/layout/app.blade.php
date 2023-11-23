4b21b2643c5942c9b039de3a4ecfe64c90ff1c3c
alemam242 <alemam242@gmail.com>
2023-11-23 22:52:04 +0600

    template update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico" />
 
     <!-- jsvectormap css -->
-    <link href="{{ asset('assets/libs') }}/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/lib') }}/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
 
     <!--Swiper slider css-->
-    <link href="{{ asset('assets/libs') }}/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/lib') }}/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
 
     <!-- Layout config Js -->
     <script src="{{ asset('assets/js') }}/layout.js"></script>
@@ -63,22 +63,22 @@
 
 
     <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets/libs') }}/simplebar/simplebar.min.js"></script>
-    <script src="{{ asset('assets/libs') }}/node-waves/waves.min.js"></script>
-    <script src="{{ asset('assets/libs') }}/feather-icons/feather.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/simplebar/simplebar.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/node-waves/waves.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/feather-icons/feather.min.js"></script>
     <script src="{{ asset('assets/js') }}/pages/plugins/lord-icon-2.1.0.js"></script>
     <script src="{{ asset('assets/js') }}/plugins.js"></script>
 
     <!-- apexcharts -->
-    <script src="{{ asset('assets/libs') }}/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/apexcharts/apexcharts.min.js"></script>
 
     <!-- Vector map-->
-    <script src="{{ asset('assets/libs') }}/jsvectormap/js/jsvectormap.min.js"></script>
-    <script src="{{ asset('assets/libs') }}/jsvectormap/maps/world-merc.js"></script>
+    <script src="{{ asset('assets/lib') }}/jsvectormap/js/jsvectormap.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/jsvectormap/maps/world-merc.js"></script>
 
     <!--Swiper slider js-->
-    <script src="{{ asset('assets/libs') }}/swiper/swiper-bundle.min.js"></script>
+    <script src="{{ asset('assets/lib') }}/swiper/swiper-bundle.min.js"></script>
 
     <!-- Dashboard init -->
     <script src="{{ asset('assets/js') }}/pages/dashboard-ecommerce.init.js"></script>
